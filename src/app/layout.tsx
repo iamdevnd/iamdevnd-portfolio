6a82e76b6f73afda99f62a25adf0e0dcecb5ea61
DN <doddanikhil@gmail.com>
2025-09-19 23:45:22 -0400

    logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -70,11 +70,15 @@ export const metadata: Metadata = {
     },
   },
   icons: {
-    icon: "/favicon.ico",
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
     shortcut: "/favicon.ico",
-    apple: "/apple-icon.png",
+    apple: "/apple-touch-icon.png",
   },
-  manifest: "/manifest.json",
+  manifest: "/site.webmanifest",
+
 }
 
 interface RootLayoutProps {
